5005c87028be60145c1d86e6bbe35e26621171b4
Yousuf <skyousuf1070@gmail.com>
2020-07-12 12:45:28 +0530

    Remove redundant code

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -1,30 +1,16 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class PangramChecker {
 
     public boolean isPangram(String input) {
-        //throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
         String lowerCaseInput = input.toLowerCase();
-        if (input.length() <= 0) {
-            return false;
-        } else {
-            HashMap <Character, Integer> hm = new HashMap<Character, Integer>();
-            for (int i=0; i<input.length(); i++) {
-                char c = input.charAt(i);
-                if(c==' ' || c=='_') {
-                    continue;
-                }
-                System.out.println(c);
-                if (!hm.containsKey(c) && ((int)c>=(int)'a' && (int)c<=(int)'z')) {
-                    hm.put(c,1);
-                }
+        ArrayList<Character> characters = new ArrayList<>();
+        for (int i = 0; i < lowerCaseInput.length(); i++) {
+            char character = lowerCaseInput.charAt(i);
+            if (!characters.contains(character) && ((int) character >= (int) 'a' && (int) character <= (int) 'z')) {
+                characters.add(character);
             }
-            System.out.println(hm.toString());
-            if (hm.size()!=26) {
-                System.out.println("Yes");
-                return false;
-            }
-            return true;
         }
+        return characters.size() == 26;
     }
 }
