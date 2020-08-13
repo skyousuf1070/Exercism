0460d772f24335c7654011d4ace63f7bd45da24e
Yousuf <skyousuf1070@gmail.com>
2020-08-13 11:03:25 +0530

    Add basic code

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,10 @@
+public class Hamming {
+    public Hamming(String leftStrand, String rightStrand) {
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int getHammingDistance() {
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+      return 0;
+    }
+}
